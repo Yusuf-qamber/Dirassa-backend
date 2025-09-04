38a24c41ac6cdb5ae1da94c916bcb0a0f6e8c4de
Yusuf qamber <yousif02.qamber@gmail.com>
2025-09-04 05:34:35 +0300

    adding frontend url to the cors

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose.connection.on("connected", () => {
 
 
 app.use(cors(
-  {  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  {  origin: process.env.CLIENT_ORIGINS,
   credentials: true,}
 ));
 app.use(cookieParser());
